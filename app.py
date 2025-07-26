7e3e27680115a1730b81f764b73b434ede027bde
indianranger <work.piyushtawde@gmail.com.com>
2025-07-26 13:06:41 +0530

    Initial commit - Medical Recommendation System project

--- app.py
+++ app.py
@@ -359,4 +359,4 @@ def update_medical_info():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run( debug=True ,host='0.0.0.0', port=5000, debug=True)
